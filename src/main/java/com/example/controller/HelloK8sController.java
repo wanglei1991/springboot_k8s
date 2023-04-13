2d54cfce69655069869a89b059b116d7a4a458e8
Lei Wang <1374215581@qq.com>
2023-04-13 17:07:53 +0800

    first commit

---
+++ src/main/java/com/example/controller/HelloK8sController.java
@@ -0,0 +1,19 @@
+package com.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/k8s")
+@Slf4j
+public class HelloK8sController {
+    @GetMapping("/hello")
+    public  String hello(@RequestParam("name") String name){
+        String msg = "Hello " + name + " : Welcome to k8s";
+        log.info("【{}】", msg);
+        return msg;
+    }
+}
