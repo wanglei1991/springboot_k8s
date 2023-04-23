56653de261a0d75913441037dcaade334b778eb4
Lei Wang <1374215581@qq.com>
2023-04-23 16:30:21 +0800

    add npe

--- src/main/java/com/example/controller/HelloK8sController.java
+++ src/main/java/com/example/controller/HelloK8sController.java
@@ -14,7 +14,6 @@ import java.nio.charset.StandardCharsets;
 public class HelloK8sController {
     @GetMapping("/hello")
     public  String hello(@RequestParam("name") String name){
-
         String msg = "Hello " + name + " : Welcome to k8s";
         String b = null;
         System.out.println(b.getBytes(StandardCharsets.UTF_8));
